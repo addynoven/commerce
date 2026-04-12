64e21a7d534f36f127787b269eebed131e90dc07
addynoven <dmcbaditya@gmail.com>
2026-04-12 18:52:03 +0530

    feat: add red logo SVG and update about page to display it

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -58,7 +58,7 @@ export default function AboutPage() {
 
         <div className="max-w-4xl mx-auto px-4 text-center relative z-10">
           <div className="flex justify-center color-[#95473C] mb-8">
-            <Image src="/about/logo.svg" alt="" width={50} height={50} />
+            <Image src="/about/logo_red.svg" alt="" width={80} height={80} />
           </div>
           <h2 className="text-3xl md:text-4xl font-bold text-neutral-900 mb-10">
             Demystifying Ayurveda
